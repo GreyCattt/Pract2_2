fe4f90ce5274b0c7cc5ea4feffe6d92032d49bfc
Ivan Rudiy <rudiy545256@gmail.com>
2024-10-08 16:00:17 +0300

    Add project files.

---
+++ Task1/Source.cpp
@@ -0,0 +1,87 @@
+﻿/*
+*  Done by:
+* Student Name: Ivan Rudiy
+* Student Group: 123
+*Pract 2.2
+* 
+*2. Given a set of sequences of values ​​A[m,n]. Replace in each sequence A[i] the minimum values ​​by the sum of the elements of this sequence.
+*/
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+	srand(static_cast<unsigned>(time(0)));
+
+	int m;
+	cout << "Enter number of sequences: ";
+	cin >> m;
+
+	int n;
+	cout << "Enter length of each sequence: ";
+	cin >> n;
+
+	vector<vector<int>> A (m);
+
+	for (int i = 0; i < m; i++) // fill this two-dimensional vector/array with random values from -99 to 99
+	{
+		A[i].resize(n);
+		for (int j = 0; j < n; j++) {
+			A[i][j] = (rand() % 199) - 99;
+		}
+	}
+
+	cout << "Generated sequences:\n";//Output results, copy code from example 
+	for (int i = 0; i < m; i++) {
+		cout << "Sequence " << setw(2) << i + 1 << ": ";
+		for (int value : A[i]) {
+			cout << setw(3) << value << " ";
+		}
+		cout << endl;
+	}
+
+
+	for (int i = 0; i < m; i++) //search min value and sum
+	{
+		int min_value = A[i][0];
+		int sum = 0;
+		for (int j = 0; j < n; j++) 
+		{	
+			sum += A[i][j];
+
+			if (A[i][j] < min_value) {
+				min_value = A[i][j];  
+			}
+
+		}
+		
+	
+		cout << "Minimum value in sequence " << i + 1 << ": " << min_value << endl;
+		cout << "sum of elements " << i + 1 << ": " << sum << endl;
+		for (int j = 0; j < n; j++) // Replace the value
+		{
+			if (A[i][j] == min_value)
+			{
+				A[i][j] = sum;
+			
+			}
+		}
+		
+	}
+	cout << "Updated sequences:\n";
+	for (int i = 0; i < m; i++) {
+		cout << "Sequence " << setw(2) << i + 1 << ": ";
+		for (int value : A[i]) {
+			cout << setw(3) << value << " ";
+		}
+		cout << endl;
+	}
+	
+
+	return 0;
+}
---
+++ Task2/Source.cpp
@@ -0,0 +1,47 @@
+﻿/*Two sequences of values ​​A[n] and B[m] are given. Replace
+in A[n] the occurrence of values ​​from B[m] by the product of elements
+subsequences of B[m].*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int> A = { 1, 4, 3 };
+    vector<int> B = { 8, 5, 2, 2, 6 };
+
+    cout << "Initial sequence A: ";
+    for (int i = 0; i < A.size(); i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    cout << "Initial sequence B: ";
+    for (int i = 0; i < B.size(); ++i) {
+        cout << B[i] << " ";
+    }
+    cout << endl;
+
+    int productB =1;
+    for (int i = 0; i < B.size(); i++) 
+    {
+        productB *= B[i];
+    
+    }
+    for (int i = 0; i < A.size(); ++i) {
+      
+        for (int j = 0; j < B.size(); ++j) {
+           
+            A[i] = productB;  
+             
+        }
+    }
+    cout << "Updated sequence A : ";
+    for (int i = 0; i < A.size(); ++i) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
